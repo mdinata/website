45f6f4a72bdcd0d3c890e4d9962edf0578973f07
mdinata <34206130+mdinata@users.noreply.github.com>
2025-01-25 17:48:30 +0700

    slider

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -4,13 +4,19 @@ import streamlit as st
 data = pd.read_excel('./pages/source.xlsx')
 unique_category = data['category'].unique()
 unique_storename = data['store_name'].unique()
+minimum_price = data['price'].min()
+maximum_price = data['price'].max()
+mean_price = data['price'].mean()
 
 selected_category = st.multiselect("Select category",options=unique_category)
 selected_store = st.multiselect("Select store",options=unique_storename)
+price_point = st.slider("Price",min_value=minimum_price,max_value=maximum_price,value=mean_price)
 
 criteria1 = data['category'].isin(selected_category) 
 criteria2 = data['store_name'].isin(selected_store)
-join_criteria =  (criteria1) & (criteria2)
+criteria3 = data['price'] <= price_point
+
+join_criteria =  (criteria1) & (criteria2) & (criteria3)
 
 criteria3 = data['price'] > 12000
 criteria4 = (data['price'] >= 12000) & (data['price'] <= 40000)
